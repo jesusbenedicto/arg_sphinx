4aa93dba19c8f90354cc214e2d3e808c26f18306
Jesus Benedicto Cirujeda <jesus.benedicto@eviden.com>
2025-06-05 15:31:11 +0200

    Minor updates in config file

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,14 @@ html_sidebars = {
     ],
 }
 
+
+# -- Sphinx Multiversion --------------------------------------------------
+# https://holzhaus.github.io/sphinx-multiversion/master/configuration.html#
+# smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
+# smv_branch_whitelist = r'^.*$'
+# smv_remote_whitelist = r'^.*$'
+
+
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
